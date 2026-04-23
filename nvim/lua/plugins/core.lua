ce7fd685b2319f256f876e5cc34f5991ffbac763
Jason Baker <jbaker@gosonar.com>
2026-04-23 08:31:58 -0500

    folding config, lazy lock

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -532,7 +532,7 @@ return {
       vim.o.foldlevel = 99
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
-      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:▾,foldsep: ,foldclose:▸]]
     end,
   },
 }
